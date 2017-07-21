c6c2c769f4acb0c2d1234752a69bd50c768b77d1
elaine0326 <1811000@stfrancisprep.org>
2017-07-21 11:28:02 -0400

    added!

---
+++ HighSchoolTextAdventure.py
@@ -0,0 +1,102 @@
+print("Welcome to High School!")
+
+popular = input("Are you popular in high school?")
+if popular == "yes":
+    print("Would you throw a large party with all your friends?")
+party = input("Will you throw a large party with all your friends?")
+if party == "yes":
+    print ("Have fun party animal!")
+if party == "no":
+        print ("Have a girls night with your friends!")
+
+if popular == "no":
+        print("Are you a quiet subtle student with a small amount of friends?")
+quiet = input("Do you hang out with friends?")
+
+if quiet == "yes":
+    print ("Go to the movies with your friends?")
+if quiet == "no":
+    print ("Will you call your friends?")
+
+Call = input("Will you call your friends?")
+if call == "yes":
+    print ("Can you hang out after 8pm")
+hangout = input("Can you hang out after 8pm?")
+if hangout == "yes":
+    print ("have fun!")
+if hangout == "no":
+    print ("Stay Home!")
+
+
+# party = input()
+#
+#
+# if popular == "no"
+#     friends = input("Would you:
+# throw a large party with all of your friends?
+# or
+# have a girls night with your friends?
+#
+# If "Throw a party with your friends"
+# Ask:
+# Would you rather get in trouble for your party but later get away with it?
+# or
+# Have a great time until the cops come?
+#
+# if "Have a girls night with your friends":
+#     say "Enjoy your night!"
+# if "Would you rather get in trouble for your party but later get away with it?"
+#     say "Have fun party animal!"
+# if "Have a great time until the cops come?"
+#     say "Hope it was worth it!"
+#
+# When no,
+# Ask "Are you a quiet subtle student with a small amount of friends?
+#
+# If yes:
+# Ask:
+# Would you:
+# Hang out with friends?
+# if yes
+# Ask:
+# Would you go to the movies with your friends?
+# if "Would you go to the movies with your friends?"
+#     say "Enjoy the movie!"
+# if no
+# Ask: "Would you call your friends?"
+# if yes
+# ask:
+# Would you be able to go out at 8pm?
+# if yes
+# have fun!
+# if no
+# stay home!
+# if no
+# "What are you going to do then?"
+#
+# if no
+# "Stay home and study for your tests that you have next week"
+#
+#
+# if no:
+#     are you a student who is ignored by everyone with no friends and no one likes you?
+#
+# if yes :
+#     Would you sit with girls you dont know?
+#     if yes:
+#         Wouldn't you would feel excluded
+#         if yes:
+#             Maybe you will have a friend!
+#         if no:
+#             Looks like tomorrow you'll eat alone!
+#     if no :
+#         Would you sit alone in the cafeteria?
+#         if yes:
+#             You tried to make friends but they don't like you!
+#         if no:
+#             Go to the library alone during your free periods!
+# if no
+# Sit alone and eat in the bathroom!
+#
+# If answers are not yes or no:
+# Say "Try again with yes or no answers only!"
